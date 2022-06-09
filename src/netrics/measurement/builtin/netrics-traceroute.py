f55a24198f78e931d9b8dfad85c322d020161f07
Kyle MacMillan <macmillan@uchicago.edu>
2022-06-09 11:40:11 -0500

    renamed builtin fns

--- src/netrics/measurement/builtin/netrics-traceroute.py
+++ src/netrics/measurement/builtin/netrics-traceroute.py
@@ -1,7 +1,13 @@
 import subprocess as sp
 import json
 import sys
-import time
+
+# Local error codes
+SUCCESS = 0
+USAGE_ERROR = 1
+BATCH_FILE = 8
+NO_REPLY = 9
+INTERNAL_ERROR = 10
 
 # Default input parameters
 PARAM_DEFAULTS = {'targets': ['google.com']}
@@ -30,7 +36,7 @@ def parse_trace_stdout(out):
 
     return res
 
-def dig_stderr_parser(exit_code):
+def parse_dig_stderr(exit_code):
     """
     Parse dig exit code and return interpretable error. Error 
     messages based on Dig man page.
@@ -38,13 +44,13 @@ def dig_stderr_parser(exit_code):
 
     res = {}
     res['exit_code'] = exit_code
-    if exit_code == 1:
+    if exit_code == USAGE_ERROR:
         res['msg'] = "Usage error"
-    elif exit_code == 8:
+    elif exit_code == BATCH_FILE:
         res['msg'] = "Couldn't open batch file"
-    elif exit_code == 9:
+    elif exit_code == NO_REPLY:
         res['msg'] = "No reply from server"
-    elif exit_code == 10:
+    elif exit_code == INTERNAL_ERROR:
         res['msg'] = "Internal error"
 
     return res
@@ -71,43 +77,37 @@ def main():
 
     stdout_res = {}
     stderr_res = {}
+    exit_code = SUCCESS
 
+    # Execute traceroutes
+    procs = []
     for dst in params['targets']:
         stdout_res[dst] = {}
         stderr_res[dst] = {}
 
         # Picks first IP addr returned by DNS lookup
         recode, out = get_ip(dst)
-        if recode != 0:
-            stderr_res[dst]['dig'] = dig_stderr_parser(recode)
+        if recode != SUCCESS:
+            stderr_res[dst]['dig'] = parse_dig_stderr(recode)
             continue
         
         trace_cmd = f'scamper -O json -I "trace -P icmp-paris -q 3 -Q {out}"'
+        p = sp.Popen(trace_cmd, stdout=sp.PIPE, stderr=sp.PIPE)
+        procs.append((dst, p))
 
-        # Performs traceroute using scamper
-        try:
-            trace_res = sp.run(trace_cmd, capture_output=True, shell=True, 
-                    check=True, timeout=15)
-        except sp.TimeoutExpired:
-            stderr_res[dst]['traceroute'] = {"exit_code": 3, "msg": "Timeout"}
-            continue
-        except sp.CalledProcessError as err:
-            stderr_res[dst]['traceroute'] = {'exit_code': err.returncode, 
-                    "msg": err.stderr.decode('utf-8')}
-            continue
+    for (dst, p) in procs:
+        p.wait()
 
-        # Parses scamper output
-        output = trace_res.stdout.decode('utf-8').split('\n')[1]
+        # Parse scamper output
+        output = p.stdout.read().decode('utf-8').split('\n')[1] 
         stdout_res[dst] = parse_trace_stdout(json.loads(output))
-        stderr_res[dst] = {"exit_code": trace_res.returncode,
-                "msg":trace_res.stderr.decode('utf-8')}
-
+        stderr_res[dst] = {"exit_code": p.returncode, 
+                "msg": p.stderr.read().decode('utf-8')}
 
     # Communicate results and errors
     json.dump(stdout_res, sys.stdout)
     json.dump(stderr_res, sys.stderr)
-
-    sys.exit(0)
+    sys.exit(exit_code)
  
 
 if __name__ == '__main__':
